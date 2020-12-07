7d04d5590dfadec5c8dad8c84b42d0768417e569
lucky <takuto.yoshikai@gmail.com>
2020-12-07 14:42:21 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author = 'Takuto Yoshikai',
     author_email = 'takuto.yoshikai@gmail.com',
     description = "sirius is an encoder/decoder of data",
-    install_requires = ['setuptools'],
+    install_requires = ['setuptools', "git+https://github.com/TakutoYoshikai/lina.git"],
     packages = find_packages(),
     entry_points={
         "console_scripts": [
